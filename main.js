87212f594326d96348775576464e5483b38dac84
nerrowcool <tim3882513@gmail.com>
2019-04-02 20:54:09 +0800

    in progress

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const log4js = require('log4js')
 log4js.configure({
-  appenders: { logger: { type: 'file', filename: 'log.log' } },
-  categories: { default: { appenders: ['logger'], level: 'trace' } }
+	appenders: { logger: { type: 'file', filename: 'log.log' } },
+	categories: { default: { appenders: ['logger'], level: 'trace' } }
 });
 const logger = log4js.getLogger('logger')
 
@@ -9,38 +9,38 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const token = "NTU3NTcwNDAyNjMzNjQ2MTA2.D3YDIw.QT9Fc2ew0uWjDFhvxmJB7L6J1gc";
 
+const model = require('./model.js');
+
 client.on('ready', () => {
-  logger.info(`Logged in as ${client.user.tag}!`);
+	var data = model.findUser();
+	console.log(`Logged in as ${client.user.tag}!`);
+	console.log('return value from function 1: '+data);
+	// logger.info(model.findUser());
 });
 
-/* get new message*/
+/* get new command*/
 client.on('message', (result) => {
-  var message = result.content;
-  var username = result.author.username;
-  var userID = result.author.id;
-  var createdAt = new Date(result.createdTimestamp);
+	var message = result.content;
+	var username = result.author.username;
+	var userID = result.author.id;
+	var createdAt = new Date(result.createdTimestamp);
 
-  logger.info(createdAt+" "+username+":\t"+message);
+	console.log(model.findUser());
+	console.log("logged");
+	logger.info(username+":\t"+message);
 });
 client.login(token);
 
 // main functions
-function userExsist(userID){
-
-}
-
 function dailyReward(userID){
-  var money = db.query({
-    sql: "SELECT money FROM `profile` WHERE `userID`=?",
-    values: [userID],
-  });
-  var amount = Math.floor((Math.random() * 10) + 1);
-  var date = new Date();
+	var money = model.findUser(userID);
+	var amount = Math.floor((Math.random() * 10) + 1);
+	var date = new Date();
 
-  return db.query({
-    sql: 'UPDATE `profile` SET `money`=? WHERE `userID`=?',
-    values: [money+amount, userID],
-  });
+	return db.query({
+		sql: 'UPDATE `profile` SET `money`=? WHERE `userID`=?',
+		values: [money+amount, userID],
+	});
 }
 
 function betting(userID){
--- model.js
+++ model.js
@@ -3,60 +3,26 @@ log4js.configure({
   appenders: { logger: { type: 'file', filename: 'log.log' } },
   categories: { default: { appenders: ['logger'], level: 'trace' } }
 });
-const logger = log4js.getLogger('logger')
+const logger = log4js.getLogger('logger');
 
 const mongoClient = require('mongodb').MongoClient;
-const mongoUri = "mongodb+srv://root:9TpXKo88kq7PNLEE@jc-occro.azure.mongodb.net/test?retryWrites=true";
-const mongo = new mongoClient(mongoUri, { useNewUrlParser: true });
+const mongoUri = "mongodb+srv://root:9TpXKo88kq7PNLEE@jc-occro.azure.mongodb.net/JC?retryWrites=true";
+const mongo = new mongoClient(mongoUri, { useNewUrlParser: true, });
 
-function insertOneMongo(tableName, data){
-    mongo.db("JC").collection(tableName).insertOne(data, function(error, result){
-        if (error) throw error;
-        logger.info(result);
-    });
-}
 
-function insertManyMongo(tableName, data){
-    mongo.db("JC").collection(tableName).insertMany(data, function(error, result){
-        if (error) throw error;
-        logger.info(result);
-    });
-}
-
-function insertUser(data){
-    mongo.connect(err => {
-        insertOneMongo("users", data)
-    });
-}
-
-function updateUser(){
-    mongo.connect(err => {
-        console.log(mongo.db("JC").collection("users"));
-        // perform actions on the collection object
-        mongo.close();
-    });
-}
-
-function insertBet(){
-    mongo.connect(err => {
-        console.log(mongo.db("JC").collection("users"));
-        // perform actions on the collection object
-        mongo.close();
-    });
-}
-
-function updateBet(){
-    mongo.connect(err => {
-        console.log(mongo.db("JC").collection("users"));
-        // perform actions on the collection object
-        mongo.close();
-    });
-}
-
-function insertReward(){
-    mongo.connect(err => {
-        console.log(mongo.db("JC").collection("users"));
-        // perform actions on the collection object
-        mongo.close();
-    });
-}
+module.exports = {
+    findUser: function (id){
+        var result = "didnot change";
+        mongo.connect((err, database) => {
+            if (err) throw err;
+            database.db("JC").collection("users").find({userID: "1"}).toArray(
+                function (err, abc){
+                    if (err) throw err;
+                    result = JSON.stringify(abc);
+                    console.log("function 1 is returning value now");
+                    return result;
+                }
+            );
+        });
+    },
+};
